ef00e8477fd6eb8d49e58d6c5b380e13427da09e
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-21 16:55:01 +0700

    Add sumDigits method

--- src/DigitSumChallenge.java
+++ src/DigitSumChallenge.java
@@ -1,6 +1,25 @@
 public class DigitSumChallenge {
     public static void main(String[] args) {
+        System.out.println(sumDigits(-1));
+    }
 
+    public static int sumDigits (int number){
+        int totalSumDigits = 0;
+        if (number >= 10){
+            while (number != 0) {
+
+                //extract the least-significant digit
+                int extractor = number % 10;
+
+                //sum all least-significant digit
+                totalSumDigits += extractor;
+
+                //discard the least-significant digit
+                number = number / 10;
+            }
+            return totalSumDigits;
+        } else {}
+        return -1;
     }
 }
 
